ae73f6c28a0a20e9ac472e21f3e78dfdd56f20d8
Priyanshi <161747006+Priyanshi-06@users.noreply.github.com>
2026-03-09 15:45:56 +0530

    Add topological sort method for game routes calculation
    Implemented a topological sorting method to calculate the number of game routes using dynamic programming.

--- Graph Algorithms/Game Routes.cpp
+++ Graph Algorithms/Game Routes.cpp
@@ -28,14 +28,47 @@ int main()
     cin >> n >> m;
 
     vector<vector<ll>> adj(n + 1);
+    vector<ll> indegree(n + 1, 0);
     for (ll i = 0; i < m; i++)
     {
         ll a, b;
         cin >> a >> b;
         adj[a].push_back(b);
+        indegree[b]++;
     }
 
-    vector<ll> dp(n + 1, -1);
-    cout << dfs(1, n, adj, dp) << endl;
+    // method - 1 : using dfs
+    //  vector<ll> dp(n + 1, -1);
+    //  cout << dfs(1, n, adj, dp) << endl;
+
+    // method - 2 : using toposort
+    vector<ll> dp(n + 1, 0);
+    dp[1] = 1;
+
+    queue<ll> q;
+    for (ll i = 1; i <= n; i++)
+    {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    while (!q.empty())
+    {
+        ll node = q.front();
+        q.pop();
+
+        for (auto it : adj[node])
+        {
+            dp[it] = (dp[it] + dp[node]) % mod;
+            if (indegree[it] > 0)
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+    }
+
+    cout << dp[n] << endl;
     return 0;
 }
